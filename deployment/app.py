475d55c512aebee8572ee1af3a9ab436b4b85b7e
SagarJiyani3010 <41732777+SagarJiyani3010@users.noreply.github.com>
2020-12-16 14:00:34 +0530

    Update app.py

--- deployment/app.py
+++ deployment/app.py
@@ -16,12 +16,7 @@ def predict():
     comment = [x for x in request.form.values()]
     comment = comment[0]
     
-    #tokenizer = Tokenizer(num_words=20000)
-    #tokenized_comment = tokenizer.texts_to_sequences(comment)
-    #padded_comment = pad_sequences(tokenized_comment, maxlen=10)
-    
     #prediction = model.predict(padded_comment)
-    
     return render_template('index.html', comment = comment)
 
 if __name__ == "__main__":
